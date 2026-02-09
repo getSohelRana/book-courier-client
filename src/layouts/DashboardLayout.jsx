2b89e848d5915f435025ce3c50e8f6d2c871a0d2
getSohelRana <getsohelinfo@gmail.com>
2026-02-10 03:16:46 +0600

    dashboard active menu and log out implement

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
-import { Link, Outlet } from "react-router";
+import { Link, NavLink, Outlet } from "react-router";
 import useAuth from "../hooks/useAuth";
 import useRole from "../hooks/useRole";
 import Loading from "../components/shared/loading/Loading";
 import { FaRegFileLines } from "react-icons/fa6";
-import { LiaFileInvoiceDollarSolid, LiaHomeSolid } from "react-icons/lia";
+import { LiaCartArrowDownSolid, LiaFileInvoiceDollarSolid, LiaHomeSolid } from "react-icons/lia";
 import ThemeSwitcher from "../providers/ThemeSwitcher";
 import { HiOutlineUserGroup } from "react-icons/hi";
-import { BiBookAdd } from "react-icons/bi";
+import { BiBookAdd, BiBookReader } from "react-icons/bi";
 import { PiAddressBook } from "react-icons/pi";
 import { SlUser } from "react-icons/sl";
 import siteLogo from "../assets/book_qourier_logo2.png"
 import Container from "../components/shared/Container";
+import { GoBook } from "react-icons/go";
+import showToast from "../utilities/showToast/showToast";
 
 const DashboardLayout = () => {
   useEffect(() => {
@@ -19,10 +21,25 @@ const DashboardLayout = () => {
     document.documentElement.setAttribute("data-theme", theme);
   }, []);
 
-  const { user, loading } = useAuth();
+  const { user, loading, setLoading, signOutUser } = useAuth();
   const { role, roleLoading } = useRole();
   console.log(user);
   if (loading || roleLoading) return <Loading />;
+
+   const handleSignOut = async () => {
+     const userName = user?.displayName || "User";
+     setLoading(true);
+
+     try {
+       await signOutUser();
+       showToast("success", `${userName} logged out successfully`);
+     } catch (error) {
+       console.error(error);
+       showToast("error", "Failed to log out");
+     } finally {
+       setLoading(false);
+     }
+   };
 
   return (
     <div className="drawer lg:drawer-open">
@@ -95,8 +112,8 @@ const DashboardLayout = () => {
                     tabIndex="-1"
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
                   >
-                    <button className="btn">
-                      <a>Logout</a>
+                    <button className="btn btn-error" onClick={handleSignOut}>
+                      Logout
                     </button>
                   </ul>
                 </div>
@@ -132,122 +149,122 @@ const DashboardLayout = () => {
           <ul className="menu w-full grow gap-6 mt-9">
             {/* List item */}
             <li>
-              <Link
+              <NavLink
                 to="/"
                 className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                 data-tip="Homepage"
               >
                 <LiaHomeSolid />
                 <span className="is-drawer-close:hidden">Homepage</span>
-              </Link>
+              </NavLink>
             </li>
 
             {/* List item */}
             <li>
               {/* user order link */}
               {user?.email && role === "user" && (
-                <Link
+                <NavLink
                   to="user/orders"
-                  className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                  className="is-drawer-close:tooltip is-drawer-close:tooltip-right "
                   data-tip="Orders"
                 >
                   <FaRegFileLines />
                   <span className="is-drawer-close:hidden">Orders</span>
-                </Link>
+                </NavLink>
               )}
               {/* admin link */}
               {user?.email && role === "admin" && (
-                <Link
+                <NavLink
                   to="admin/all-users"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="All users"
                 >
                   <HiOutlineUserGroup />
                   <span className="is-drawer-close:hidden">All users</span>
-                </Link>
+                </NavLink>
               )}
               {/* librarian link */}
               {user?.email && role === "librarian" && (
-                <Link
+                <NavLink
                   to="librarian/add-book"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Add-book"
                 >
                   <BiBookAdd />
                   <span className="is-drawer-close:hidden">Add-book</span>
-                </Link>
+                </NavLink>
               )}
             </li>
             {/* 2nd links item */}
             <li>
               {/* user link */}
               {user?.email && role === "user" && (
-                <Link
+                <NavLink
                   to="user/invoices"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Orders"
                 >
                   <LiaFileInvoiceDollarSolid />
                   <span className="is-drawer-close:hidden">invoices</span>
-                </Link>
+                </NavLink>
               )}
               {/* admin link */}
               {user?.email && role === "admin" && (
-                <Link
+                <NavLink
                   to="admin/manage-books"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Manage-books"
                 >
                   <PiAddressBook />
                   <span className="is-drawer-close:hidden">Manage-books</span>
-                </Link>
+                </NavLink>
               )}
               {/* librarian link */}
               {user?.email && role === "librarian" && (
-                <Link
+                <NavLink
                   to="librarian/my-books"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="My-book"
                 >
                   <GoBook />
                   <span className="is-drawer-close:hidden">My-books</span>
-                </Link>
+                </NavLink>
               )}
             </li>
             {/* 3rd links item */}
             <li>
               {/* user link */}
               {user?.email && role === "user" && (
-                <Link
+                <NavLink
                   to="user/profile"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Profile"
                 >
                   <SlUser />
                   <span className="is-drawer-close:hidden">profile</span>
-                </Link>
+                </NavLink>
               )}
               {/* admin link */}
               {user?.email && role === "admin" && (
-                <Link
+                <NavLink
                   to="admin/profile"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Profile"
                 >
                   <SlUser />
                   <span className="is-drawer-close:hidden">Profile</span>
-                </Link>
+                </NavLink>
               )}
               {/* librarian link */}
               {user?.email && role === "librarian" && (
-                <Link
+                <NavLink
                   to="librarian/my-orders"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="My-orders"
                 >
-                  <BiBookReader />
+                  <LiaCartArrowDownSolid />
                   <span className="is-drawer-close:hidden">My-orders</span>
-                </Link>
+                </NavLink>
               )}
             </li>
           </ul>
