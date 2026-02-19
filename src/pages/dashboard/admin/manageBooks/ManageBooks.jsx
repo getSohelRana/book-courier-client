a299d7c991d8a578d02708fb34a53cdbe4ba2317
getSohelRana <getsohelinfo@gmail.com>
2026-02-19 20:00:48 +0600

    admin book and that book order delete functionality implement

--- src/pages/dashboard/admin/manageBooks/ManageBooks.jsx
+++ src/pages/dashboard/admin/manageBooks/ManageBooks.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Container from "../../../../components/shared/Container";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useAuth from "../../../../hooks/useAuth";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import axios from "axios";
 import Loading from "../../../../components/shared/loading/Loading";
+import Swal from "sweetalert2";
 
 const ManageBooks = () => {
   const { user } = useAuth();
-
+  const queryClient = useQueryClient();
+  // load all books data
   const { data: books = [], isLoading } = useQuery({
     queryKey: ["book-collection", user?.email],
     queryFn: async () => {
@@ -21,12 +23,48 @@ const ManageBooks = () => {
       return res.data.data;
     },
   });
-  console.log(books);
+  // console.log(books);
+
+  // delete mutation
+  const { mutate: deleteBookData } = useMutation({
+    mutationFn: async (id) => {
+      const res = await axios.delete(
+        `${import.meta.env.VITE_api_url}/books/${id}`,
+      );
+      return res.data;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(["book-collection", user?.email]);
+      Swal.fire({
+        title: "Deleted!",
+        text: "The book and all related orders have been deleted successfully.",
+        icon: "success",
+      });
+    },
+
+    onError: () => {
+      Swal.fire("Error!", "Delete failed!", "error");
+    },
+  });
 
   // handle delete book
   const handleDeleteBook = (id) => {
-    console.log("delete btn clicked", id)
-  }
+    // console.log("delete btn clicked", id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "This book will be deleted!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#234c6a",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteBookData(id);
+      }
+    });
+  };
 
   if (isLoading) return <Loading></Loading>;
   return (
@@ -65,15 +103,15 @@ const ManageBooks = () => {
                       <div className="avatar">
                         <div className="mask mask-squircle h-12 w-12">
                           <img
-                            src={book.addedBy.photo}
-                            alt={book.addedBy.name}
+                            src={book.addedBy?.photo}
+                            alt={book.addedBy?.name}
                           />
                         </div>
                       </div>
                       <div>
-                        <div className="font-bold">{book.addedBy.name}</div>
+                        <div className="font-bold">{book.addedBy?.name}</div>
                         <div className="text-sm opacity-50">
-                          {book.addedBy.email}
+                          {book.addedBy?.email}
                         </div>
                       </div>
                     </div>
@@ -94,7 +132,11 @@ const ManageBooks = () => {
                     </select>
                   </td>
                   <td>
-                    <button type="button" onClick={() => handleDeleteBook(book._id )} className="btn bg-transparent shadow-none border-0 text-2xl text-error">
+                    <button
+                      type="button"
+                      onClick={() => handleDeleteBook(book._id)}
+                      className="btn bg-transparent shadow-none border-0 text-2xl text-error"
+                    >
                       <RiDeleteBin6Line />
                     </button>
                   </td>
