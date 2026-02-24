d7656f27ef9c125be0b5f1d6743c3c7037081817
getSohelRana <getsohelinfo@gmail.com>
2026-02-24 10:36:48 +0600

    fix search and sort functionality

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -8,40 +8,36 @@ import { FaSearch } from "react-icons/fa";
 
 const AllBooks = () => {
   const [search, setSearch] = useState("");
-
+  const [sort, setSort] = useState("");
   // load book data
-  const { data: allBooks = [], isLoading } = useQuery({
-    queryKey: ["all-books"],
+  const {
+    data: allBooks = [],
+    isLoading,
+    isFetching,
+  } = useQuery({
+    queryKey: ["all-books", search, sort],
     queryFn: async () => {
-      const res = await axios(`${import.meta.env.VITE_api_url}/all-books`);
+      const params = {};
+
+      if (search) params.search = search;
+      if (sort) params.sort = sort;
+
+      const res = await axios.get(`${import.meta.env.VITE_api_url}/search`, {
+        params,
+      });
+
       return res.data.data;
     },
   });
   // console.log(allBooks);
-  // search data load
-  const { data: searchData = [], isFetching } = useQuery({
-    queryKey: ["search", search],
-    queryFn: async () => {
-      const res = await axios.get(
-        `${import.meta.env.VITE_api_url}/search?search=${search}`,
-      );
-      return res.data.data;
-    },
-    enabled: !!search,
-  });
-
-  // console.log(searchData);
 
   // handle search
   const handleSearch = (e) => {
     e.preventDefault();
-    const search = e.target.search.value.trim();
-    console.log(search);
-    setSearch(search);
+    const value = e.target.search.value.trim();
+    setSearch(value);
     e.target.reset();
   };
-
-  const booksToShow = search ? searchData : allBooks;
 
   if (isLoading) return <Loading></Loading>;
   return (
@@ -49,14 +45,17 @@ const AllBooks = () => {
       <div className="my-10">
         <title>All books</title>
         <h1 className="heading_title ">
-          Currently {booksToShow.length} Book Found{" "}
+          Currently {allBooks.length} Book Found{" "}
         </h1>
       </div>
       <div className="flex flex-col md:flex-row gap-4 justify-between items-center mb-8">
-
         {/* Sort */}
-        <select className="select w-full md:w-52">
-          <option disabled>Sort By</option>
+        <select
+          className="select w-full md:w-52"
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
+        >
+          <option value="">Sort By</option>
           <option value="desc">Price: High → Low</option>
           <option value="asc">Price: Low → High</option>
         </select>
@@ -78,12 +77,9 @@ const AllBooks = () => {
         </form>
       </div>
 
-      {/* search loading spinner */}
-      {isFetching && <Loading></Loading>}
-
-      {booksToShow && booksToShow.length > 0 ? (
+      {allBooks && allBooks.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-          {booksToShow.map((allBook) => (
+          {allBooks.map((allBook) => (
             <Card key={allBook._id} allBook={allBook}></Card>
           ))}
         </div>
